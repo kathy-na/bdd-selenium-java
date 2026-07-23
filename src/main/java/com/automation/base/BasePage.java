b4bf1b1f5963bb95f5be39497b6e9a64605d1df4
kathy <wila21@naver.com>
2026-07-24 00:09:20 +0900

    Changing the function to wait until it becomes clickable

--- src/main/java/com/automation/base/BasePage.java
+++ src/main/java/com/automation/base/BasePage.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
@@ -26,7 +27,12 @@ public class BasePage {
 	}
 	
 	protected void click(By locator) {
-		driver.findElement(locator).click();
+		WebDriverWait wait =
+	            new WebDriverWait(driver, Duration.ofSeconds(5));
+
+	    wait.until(
+	        ExpectedConditions.elementToBeClickable(locator)
+	    ).click();
 	}
 	
 	
